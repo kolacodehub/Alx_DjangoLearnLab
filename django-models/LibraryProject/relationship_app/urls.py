96ffcb7b5e4105c028a03772bda11694da73a15b
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-01-22 03:17:36 +0100

    Add views and templates for admin, librarian, and member dashboards

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -18,4 +18,7 @@ urlpatterns = [
     path(
         "library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_details"
     ),
+    path("admin_view/", views.admin_view, name="admin_view"),
+    path("librarian_view/", views.librarian_view, name="librarian_view"),
+    path("member_view/", views.member_view, name="member_view"),
 ]
