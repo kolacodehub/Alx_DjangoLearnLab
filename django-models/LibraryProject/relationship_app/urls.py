6b5919ecda509da3cff409e713d437cc2cd45e21
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-01-22 02:22:31 +0100

    Refactor URL patterns to improve readability and maintainability to (from . import views)

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import list_books, LibraryDetailView, register
+from . import views
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(template_name="relationship_app/login.html"), name='login'),
-    path("logout/", auth_views.LogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
-    path("register/", register, name="register"),
-    path("books/", list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_details"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="relationship_app/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="relationship_app/logout.html"),
+        name="logout",
+    ),
+    path("register/", views.register, name="register"),
+    path("books/", views.list_books, name="list_books"),
+    path(
+        "library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_details"
+    ),
 ]
