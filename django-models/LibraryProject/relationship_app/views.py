fdc7dc6f12c8f21bc2ea8fd2db9529a380db5cbd
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-01-22 01:34:43 +0100

    relationship_app added to template name

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,10 +7,10 @@ from django.views.generic import DetailView
 def list_books(request):
     books = Book.objects.all()
     context = {"books": books}
-    return render(request, "list_books.html", context)
+    return render(request, "relationship_app/list_books.html", context)
 
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
