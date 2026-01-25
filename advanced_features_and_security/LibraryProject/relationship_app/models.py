064227aad0001139af8914def0ed45c7a5befadb
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-01-25 22:56:07 +0100

    Remove unused CustomUserManager class and related imports from models.py

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,18 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
 # Create your models here.
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        
-
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
