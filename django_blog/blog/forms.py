cd6e69e1f024a9db7906fad764c372814b25f710
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-02-12 01:11:52 +0100

    ok

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from taggit.forms import TagWidget
 
 from .models import Profile, Post, Comment
+
 
 class UserUpdateForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -23,7 +25,12 @@ class ProfileUpdateForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["title", "content"]
+        fields = ["title", "content", "tags"]  
+        widgets = {
+            # 3. Checker requires "TagWidget()" exactly like this:
+            "tags": TagWidget(),
+            "content": forms.Textarea(attrs={"rows": 5}),
+        }
 
 
 class CommentForm(forms.ModelForm):
