efac4d24c832e06006b96b72addc6bf0e1ae14af
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-02-20 00:38:33 +0100

    Create Post and Comment Models

--- social_media_api/posts/admin.py
+++ social_media_api/posts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "truncated_content", "created_at")
+    search_fields = ("title", "content")
+
+    @admin.display(description="Content Preview")
+    def truncated_content(self, obj):
+        if len(obj.content) > 50:
+            return f"{obj.content[:50]}..."
+        return obj.content
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("author", "post", "truncated_comment", "created_at")
+    search_fields = ("content",)
+
+    @admin.display(description="Comment Excerpt")
+    def truncated_comment(self, obj):
+        if len(obj.content) > 30:
+            return f"{obj.content[:30]}..."
+        return obj.content
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
