5727ed08e5fb27f6360620904b7cf2216ada80d1
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-02-20 10:19:44 +0100

    Create Views for CRUD Operations

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsAuthorOrReadOnly
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    # IsAuthorOrReadOnly: Our custom bouncer ensures only the author can edit/delete.
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    # This intercepts the save process to inject the author automatically
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
