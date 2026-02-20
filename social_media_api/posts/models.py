a96bce2c18119ed7c5325235c3e48d7fa9d24bb3
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-02-20 12:11:30 +0100

    Create Notification app and create Like and Notification Models

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.conf import settings
+
 
 User = get_user_model()
 
@@ -26,3 +28,15 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("post", "user")  # Prevents double-liking
+
+    def __str__(self):
+        return f"{self.user} liked {self.post.title}"
