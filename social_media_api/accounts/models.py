8c286aed4a5db3110407e29fa139b068c248ca6f
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-02-20 11:23:45 +0100

    Edited my Models.py in accounts app

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -7,8 +7,8 @@ class CustomUser(AbstractUser):
     profile_picture = models.ImageField(
         upload_to="profile_pics/", blank=True, null=True
     )
-    followers = models.ManyToManyField(
-        "self", symmetrical=False, related_name="following", blank=True
+    following = models.ManyToManyField(
+        "self", symmetrical=False, related_name="followers", blank=True
     )
 
     def __str__(self):
